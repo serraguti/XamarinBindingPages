7035117e265d0ffba7d7d89be37a4d744cb7824b
serraguti <pacoserranox@gmail.com>
2022-05-24 13:58:14 +0200

    commit

--- XamarinBindingPages/XamarinBindingPages/ViewModels/PersonajeViewModel.cs
+++ XamarinBindingPages/XamarinBindingPages/ViewModels/PersonajeViewModel.cs
@@ -9,6 +9,26 @@ namespace XamarinBindingPages.ViewModels
 {
     public class PersonajeViewModel : INotifyPropertyChanged
     {
+        public PersonajeViewModel()
+        {
+            Personaje personaje = new Personaje();
+            personaje.Nombre = "Steve Harrington";
+            personaje.Serie = "Stranger Things";
+            personaje.Imagen = "https://i.pinimg.com/736x/cd/08/7e/cd087ef80bd96e63910f02b7c2d222da.jpg";
+            this.Personaje = personaje;
+        }
+
+        private string _Nombre;
+        public string Nombre
+        {
+            get { return this._Nombre; }
+            set
+            {
+                this._Nombre = value;
+                RaisePropertyChanged("Nombre");
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         public void RaisePropertyChanged(string propertyName)
         {
@@ -35,11 +55,35 @@ namespace XamarinBindingPages.ViewModels
                 return new Command(() =>
                 {
                     Personaje personaje = new Personaje();
-                    personaje.Nombre = "Dustin Henderson";
+                    personaje.Nombre = this.Nombre;
                     personaje.Serie = "Stranger Things";
                     personaje.Imagen = "https://i.pinimg.com/236x/cf/76/9e/cf769e18285f2706b457a4dd845db707.jpg";
                     this.Personaje = personaje;
                 });
+            }
+        }
+
+        private string _Descripcion;
+
+        public string Descripcion
+        {
+            get { return this._Descripcion; }
+            set {
+                this._Descripcion = value;
+                RaisePropertyChanged("Descripcion");
+            }
+        }
+
+        public Command MostrarDescripcion
+        {
+            get
+            {
+               return new Command(() =>
+               {
+                   this.Descripcion = this.Personaje.Nombre + ", "
+                   + this.Personaje.Serie;
+                   RaisePropertyChanged("Personaje");
+               });
             }
         }
     }
